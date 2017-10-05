89555e98bbd8bc17cf6a5ed52d2749a784e467f0
Bruno Scheltzke <brunofonten@gmail.com>
2017-10-05 17:25:31 -0300

    added second triangle

--- Tangram/TangramBoard.swift
+++ Tangram/TangramBoard.swift
@@ -23,19 +23,30 @@ class TangramBoard: UIView {
     func createLayers() {
         let triangle1 = CAShapeLayer()
         triangle1.frame = layer.bounds
-        let triangle1Width = frame.width/2
-        let triangle1Height = frame.height
         
         let path = UIBezierPath()
         path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: triangle1Width, y: triangle1Height/2))
-        path.addLine(to: CGPoint(x: 0, y: triangle1Height))
+        path.addLine(to: CGPoint(x: frame.width/2, y: frame.height/2))
+        path.addLine(to: CGPoint(x: 0, y: frame.height))
         path.close()
         
         triangle1.path = path.cgPath
-        triangle1.lineWidth = 2.0
         triangle1.fillColor = UIColor.red.cgColor
         
         self.layer.addSublayer(triangle1)
+        
+        let triangle2 = CAShapeLayer()
+        triangle2.frame = layer.bounds
+        
+        let path2 = UIBezierPath()
+        path2.move(to: CGPoint(x: 0, y: 0))
+        path2.addLine(to: CGPoint(x: frame.width/2, y: frame.height/2))
+        path2.addLine(to: CGPoint(x: frame.width, y: 0))
+        path2.close()
+        
+        triangle2.path = path2.cgPath
+        triangle2.fillColor = UIColor.black.cgColor
+        
+        self.layer.addSublayer(triangle2)
     }
 }
